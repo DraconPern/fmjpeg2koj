e4b714265bc74a73a9e0a397ac3bc1e8686df4f6
DraconPern <draconpern@hotmail.com>
2015-09-13 21:06:29 -0500

    build script added

---
+++ build.bat
@@ -0,0 +1,26 @@
+SET TYPE=Release
+SET TYPE=Debug
+
+REM a top level directory for all PACS related code
+SET DEVSPACE="%CD%"
+
+cd %DEVSPACE%\dcmtk
+mkdir build-%TYPE%
+cd build-%TYPE%
+cmake .. -G "Visual Studio 11" -DDCMTK_WIDE_CHAR_FILE_IO_FUNCTIONS=1 -DCMAKE_INSTALL_PREFIX=%DEVSPACE%\dcmtk\%TYPE%
+msbuild /P:Configuration=%TYPE% INSTALL.vcxproj 
+cd ..\..
+
+cd %DEVSPACE%\openjpeg
+mkdir build-%TYPE%
+cd build-%TYPE%
+cmake .. -G "Visual Studio 11" -DBUILD_THIRDPARTY=1 -DBUILD_SHARED_LIBS=0 -DCMAKE_C_FLAGS_RELEASE="/MT /O2 /D NDEBUG" -DCMAKE_C_FLAGS_DEBUG="/D_DEBUG /MTd /Od" -DCMAKE_INSTALL_PREFIX=%DEVSPACE%\openjpeg\%TYPE%
+msbuild /P:Configuration=%TYPE% INSTALL.vcxproj
+
+cd %DEVSPACE%
+mkdir build-%TYPE%
+cd build-%TYPE%
+cmake .. -G "Visual Studio 11" -DOPENJPEG=%DEVSPACE%\openjpeg\%TYPE% -DDCMTK_DIR=%DEVSPACE%\dcmtk\build-%TYPE% -DCMAKE_INSTALL_PREFIX=%DEVSPACE%\%TYPE%
+msbuild /P:Configuration=%TYPE% INSTALL.vcxproj
+
+cd %DEVSPACE%
